28c9e70846baae8cbcfbc02385640bda94567f01
Kovenant16 <reimer1693@gmail.com>
2025-05-06 01:01:06 -0500

    añadido verificacion y añadidura de clientes

--- routes/clienteRoutes.js
+++ routes/clienteRoutes.js
@@ -1,11 +1,15 @@
-import express from "express";
-
-import { agregarCliente,buscarClientesPorTelefono } from "../controllers/clienteController.js";
-import checkAuth from "../middleware/checkAuth.js";
+import express from 'express';
+import {
+    registrarCliente,
+    buscarClientesPorTelefono,
+    verificarCodigoCliente // Asegúrate de importar esta función
+} from '../controllers/clienteController.js';
+import checkAuth from '../middleware/checkAuth.js'; // Si tienes middleware de autenticación
 
 const router = express.Router();
 
-router.get("/", checkAuth, agregarCliente);
-router.post("/buscarClientesPorTelefono", checkAuth, buscarClientesPorTelefono)
+router.post("/registrar", registrarCliente);
+router.post("/buscar", buscarClientesPorTelefono);
+router.post("/verificar-codigo", verificarCodigoCliente); // Nueva ruta para verificar el código
 
 export default router;
