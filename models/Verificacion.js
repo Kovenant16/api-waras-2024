b76f17d11eeefccd19deb84056bb469c5f958035
Kovenant16 <reimer1693@gmail.com>
2025-05-07 01:49:23 -0500

    modificado modelo de verificacion

--- models/Verificacion.js
+++ models/Verificacion.js
@@ -1,9 +1,24 @@
 import mongoose from 'mongoose';
 
 const verificacionSchema = new mongoose.Schema({
-    telefono: { type: String, required: true, unique: true },
-    codigo: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now, expires: '5m' }, // El código expirará en 5 minutos
+    telefono: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    codigo: {
+        type: String,
+        required: true
+    },
+    expireAt: { // Agrega este campo al esquema
+        type: Date,
+        required: true,
+        index: { expires: '5m' } // Establece el tiempo de expiración (5 minutos en este ejemplo)
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
 });
 
 const Verificacion = mongoose.model('Verificacion', verificacionSchema);
